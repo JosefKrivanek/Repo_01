163c4cee7b93d9066bd1b5a2627a6c6b8f9f9d87
JosefKrivanek <kri41387@spseol.cz>
2023-10-25 01:11:57 +0200

    Update Gamesa.py

--- Gamesa.py
+++ Gamesa.py
@@ -1,1 +1,24 @@
-print("Murder case")
+import random
+import math
+import time
+
+choice = 0
+##############################################################################
+input("Murder case \nPress Enter to continue")
+
+input("\033[91m. . .\nOnce upon the time...\033[0m")
+##############################################################################
+choice = int(input("\033[91mgrrrrr he said \n1 - kill\n0 - save\n \033[0m"))
+if choice == 1:
+    time.sleep(2)
+    input("kill")
+    choice = 0
+    print (choice)
+elif choice == 0:
+    input("save")
+    print(choice)
+
+
+
+
+
